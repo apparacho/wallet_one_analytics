f6ebe011eea609c0e5e7598f4db7fe1be1974320
Artyom Nekhoroshev <a.nekhoroshev@uiscom.ru>
2019-05-23 19:43:58 +0300

    AN-31 - Интеграция АПИ. Коллекция отчетов
                Добавил отображение показателей

--- src/components/reports/report-view.js
+++ src/components/reports/report-view.js
@@ -5,6 +5,16 @@ import { reportSelector, fetchReport } from '../../ducks/reports'
 import { Table } from 'antd'
 import { addIdByIndex } from '../../ducks/utils'
 
+const indicatorsColumns = [
+    {
+        title: 'Показатель',
+        dataIndex: 'name'
+    },
+    {
+        title: 'Значение',
+        dataIndex: 'value'
+    }
+];
 
 class ReportView extends Component {
     static propTypes = {
@@ -19,12 +29,24 @@ class ReportView extends Component {
         const columns = this.props.report.templateColumns.map(col => ({ dataIndex: col.columnName, title: col.humanReadableName }))
         return (
             <div>
-                <h4>{this.props.report.name}</h4>
-                <Table
-                    style={{ backgroundColor: '#fff'}}
-                    pagination={{size: "small", showSizeChanger: true }}
-                    columns={columns}
-                    dataSource={addIdByIndex(this.props.report.table)} />
+                <div>
+                    <h4>{this.props.report.name}</h4>
+                    <Table
+                        style={{ backgroundColor: '#fff'}}
+                        pagination={{size: "small", showSizeChanger: true }}
+                        columns={columns}
+                        dataSource={addIdByIndex(this.props.report.table)}
+                    />
+                </div>
+                <div style={{ marginTop: '25px'}}>
+                    <h4>Показатели</h4>
+                    <Table
+                        style={{ backgroundColor: '#fff'}}
+                        pagination={{size: "small", showSizeChanger: true }}
+                        columns={indicatorsColumns}
+                        dataSource={addIdByIndex(this.props.report.indicators)}
+                    />
+                </div>
             </div>
           )
     }
